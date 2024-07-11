f904ecf1162c9d1e4f15611031a4099d5a7edc98
mitesh virash <miteshkvirash@gmail.com>
2024-07-11 23:12:24 +0530

    validations added for create topic page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,21 @@
-export default function ShowTopicPage() {
-  return <div>topic</div>;
+import CreatePostForm from "@/components/posts/create-post-form";
+
+interface ShowTopicProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ShowTopicPage({ params }: ShowTopicProps) {
+  const { slug } = params;
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+      </div>
+      <div>
+        <CreatePostForm />
+      </div>
+    </div>
+  );
 }
