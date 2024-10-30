d351ab49f293f27999abb1fee420bf48f129ac40
mitesh virash <miteshkvirash@gmail.com>
2024-10-30 16:15:06 +0530

    search fn implemented

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,4 @@ export { createComment } from "./create-comment";
 export { createTopic } from "./create-topic";
 export { signIn } from "./sign-in";
 export { signOut } from "./sign-out";
+export { search } from "./search";
